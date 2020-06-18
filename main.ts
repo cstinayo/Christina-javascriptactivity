2d3e7e233ad9f776512f053aac28b8d031203af4
cstinayo <66885051+cstinayo@users.noreply.github.com>
2020-06-18 13:28:28 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+
+// This function prints out your name and favorite color 
+function nameAndColor (name:string, color:string){
+    console.log(" Hello " + name + ". Your Favorite Color is " + color)
+}
+
+nameAndColor ("Christina","violet" )
+
+
+// This function prints out your name and favorite number
+function nameAndNumber(name:string, favNum:number){
+    console.log(" Hello " + name + ", Your favorite number is" + favNum)
+}
+
+nameAndNumber("Christina", 10)
+
+
